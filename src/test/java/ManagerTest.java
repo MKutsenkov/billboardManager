23b147b235340c0b0eda93fceeae81a5ed75466c
Kitsenkov Mikhail <km36rus@gmail.com>
2022-10-19 12:15:49 +0300

    "+2 теста"

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -15,6 +15,7 @@ public class ManagerTest {
     List film8 = new List(18, 8, "Фильм8", 300, 6);
     List film9 = new List(19, 9, "Фильм9", 300, 4);
     List film10 = new List(20, 10, "Фильм10", 100, 2);
+    List film11 = new List(21, 11, "Фильм11", 200, 4);
 
     @Test
     public void testFindLast() {
@@ -36,4 +37,62 @@ public class ManagerTest {
         assertArrayEquals(actual, expected);
     }
 
+    @Test
+    public void testFindLastLessLimit() {
+        ManagerFilm manager = new ManagerFilm();
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+        manager.add(film4);
+        manager.add(film5);
+        manager.add(film6);
+        manager.add(film7);
+        manager.add(film8);
+        manager.add(film9);
+
+        List[] actual = manager.findLast();
+        List[] expected = {film9, film8, film7, film6, film5, film4, film3, film2, film1};
+
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void testFindLastMoreLimit() {
+        ManagerFilm manager = new ManagerFilm();
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+        manager.add(film4);
+        manager.add(film5);
+        manager.add(film6);
+        manager.add(film7);
+        manager.add(film8);
+        manager.add(film9);
+        manager.add(film10);
+        manager.add(film11);
+        List[] actual = manager.findLast();
+        List[] expected = {film11, film10, film9, film8, film7, film6, film5, film4, film3, film2};
+
+        assertArrayEquals(actual, expected);
+    }
+
+//   @Test
+//   public void testFindLastMoreLimitBag() {
+//       ManagerFilm manager = new ManagerFilm();
+//       manager.add(film1);
+//       manager.add(film2);
+//       manager.add(film3);
+//       manager.add(film4);
+//       manager.add(film5);
+//       manager.add(film6);
+//       manager.add(film7);
+//       manager.add(film8);
+//       manager.add(film9);
+//       manager.add(film10);
+//       manager.add(film11);
+//       List[] actual = manager.findLast();
+//       List[] expected = {film11, film10, film9, film8, film7, film6, film5, film4, film3, film2, film1};
+
+//       assertArrayEquals(actual, expected);
+//   }
 }
